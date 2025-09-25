469d1c4eb171f79e865be0a1564947f440eeb3eb
zebra <victoras.isaakidis@sysdocgroup.com>
2025-09-25 15:30:30 +0300

    added time trigger for always on server

---
+++ Functions/AlwaysOnTimerTrigger.cs
@@ -0,0 +1,33 @@
+using Microsoft.Azure.Functions.Worker;
+
+namespace CompanySearchBackend.Functions
+{
+    public class AlwaysOnTimerTrigger(ILoggerFactory loggerFactory)
+    {
+        private readonly ILogger _logger = loggerFactory.CreateLogger<AlwaysOnTimerTrigger>();
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        private const string BackendUrl = "https://companysearchalwayson.azurewebsites.net/api/company/open";
+
+        [Function("AlwaysOnTimerTrigger")]
+        public async Task Run([TimerTrigger("0 */18 * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation("Keep-alive ping executed at: {executionTime}", DateTime.Now);
+
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation("Next schedule at: {nextSchedule}", myTimer.ScheduleStatus.Next);
+            }
+
+            try
+            {
+                var response = await HttpClient.GetAsync(BackendUrl);
+                _logger.LogInformation("Pinged {url} - Status: {status}", BackendUrl, response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error pinging {url}: {message}", BackendUrl, ex.Message);
+            }
+        }
+    }
+}
